3fe316643ed14a2e70804ebd248100afdbc9f6d5
ImEldie <daniel.frankort@telenet.be>
2024-12-29 21:32:57 +0100

    Feature: Add basic print commands function

--- bupcli/__main__.py
+++ bupcli/__main__.py
@@ -4,30 +4,34 @@ from .commands import add_path
 from .commands import list_paths
 from .commands import del_path
 
+COMMAND_MAPPING = {
+    'add': add_path.run,
+    'a': add_path.run,
+    'del': del_path.run,
+    'd': del_path.run,
+    'list': list_paths.run,
+    'l': list_paths.run,
+}
+
 def main():
     args = sys.argv[1:]
     if len(args) == 0:
-        # TODO -> Print help command or version or something :-)
-        print('No arguments given.')
+        print_command_mapping()
         return
-
-    command_mapping = {
-        'add': add_path.run,
-        'a': add_path.run,
-
-        'del': del_path.run,
-        'd': del_path.run,
-
-        'list': list_paths.run,
-        'l': list_paths.run,
-    }
     
+    global COMMAND_MAPPING
     command = args[0].lower()
-    action = command_mapping.get(command)
+    action = COMMAND_MAPPING.get(command)
     if action:
         action(args)
     else:
         print(f"Unknown command: {command}")
-    
+
+def print_command_mapping():
+    global COMMAND_MAPPING
+    print("Available commands:")
+    for command in COMMAND_MAPPING.keys():
+        print(f"- {command}")
+
 if __name__ == '__main__':
     main()
