5578650ff049db3786e1e7a1aa0e4d6f1fb7a83d
ImEldie <daniel.frankort@telenet.be>
2025-01-06 16:29:18 +0100

    Feature: Only backup folders that have changes, add hashing to check for changes & remove backups_old folder system

--- bupcli/config/__config__.py
+++ bupcli/config/__config__.py
@@ -5,3 +5,10 @@ def get_config_path():
     config_dir = Path.home() / folder_name
     config_dir.mkdir(exist_ok=True)
     return config_dir
+
+def get_hashes_path():
+    config_path = get_config_path()
+    folder_name = "hash"
+    hash_dir = config_path / folder_name
+    hash_dir.mkdir(exist_ok=True)
+    return hash_dir
--- bupcli/functions/backup_folder.py
+++ bupcli/functions/backup_folder.py
@@ -2,11 +2,13 @@ import shutil
 import os
 import zipfile
 import concurrent.futures
+import hashlib
 
 from pathlib import Path
 from tqdm import tqdm
-from  ..config import settings_manager as settings
-from  ..config import paths_manager as paths
+from ..config import settings_manager as settings
+from ..config import paths_manager as paths
+from ..config import __config__ as config
 
 def backup_all():
     dst_parent = settings.get_backup_directory()
@@ -19,67 +21,69 @@ def backup_all():
         print("No directories added yet, add directories using `bup add <folder_path>`")
         return
 
-    num_dirs = len(dirs)
+    print("Checking directories...")
+    valid_dirs = []
+    for path in dirs:
+        print(f"* '{path}'")
+        path = Path(path)
+        if not os.path.isdir(path):
+            print(f"   !!! Does not exist !!!")
+        elif not _folder_has_changes(path):
+            print(f"   - No changes since last backup.")
+        else:
+            print("   - Changes detected.")
+            valid_dirs.append(path)
+
+    num_dirs = len(valid_dirs)
     dst_path = _prepare_and_get_dst_path(dst_parent)
     num_workers = _get_optimal_workers(num_dirs)
 
+    if num_dirs == 0:
+        print("No files to back-up.")
+        return
+    
+    print()
     if num_dirs == 1:
-        print(f"Starting BUP for 1 directory using {num_workers} workers:")
+        print(f"Starting Backup for 1 directory using {num_workers} workers:")
     else:
-        print(f"Starting BUP for {num_dirs} directories using {num_workers} workers:")
+        print(f"Starting Backup for {num_dirs} directories using {num_workers} workers:")
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
-        futures = [executor.submit(_backup, Path(src), dst_path) for src in dirs]
+        futures = [executor.submit(_backup, Path(src), dst_path) for src in valid_dirs]
         for future in concurrent.futures.as_completed(futures):
             try:
                 future.result()
             except Exception as e:
                 print(f"Error during backup: {e}")
 
+    print()
     print("Backups completed.")
 
 def _get_optimal_workers(num_dirs):
     cpu_cores = os.cpu_count()
-
-    # If there are more directories than CPU cores, use the number of directories
-    # Otherwise, use a fraction of CPU cores or the number of cores
-    if num_dirs > cpu_cores:
-        return min(cpu_cores, num_dirs)
-    else:
-        return cpu_cores
+    return min(num_dirs, cpu_cores)
 
 def _prepare_and_get_dst_path(dst_parent):
     folder_name = "Backups"
     dst_path = Path(dst_parent) / folder_name
-
-    if dst_path.exists():
-        old_folder_name = f"{folder_name}_Old"
-        old_dst_path = Path(dst_parent) / old_folder_name
-    
-        if old_dst_path.exists():
-            try:
-                shutil.rmtree(old_dst_path)
-            except OSError as e:
-                print(f"Error deleting old backup folder: {e.strerror}")
-        
-        try:
-            shutil.move(str(dst_path), str(old_dst_path))
-        except Exception as e:
-            print(f"Error renaming folder: {e}")
     
     dst_path.mkdir(parents=True, exist_ok=True)
     return dst_path
 
 def _backup(src_path, dst_path):
-    zip_name =  str(src_path).replace("\\", "_").replace("/", "_").replace("__", "_")
-    zip_name = _cleanup_path_string(zip_name)
-    filename = f"{zip_name}.zip"
+    filename = _convert_to_filename(src_path, "zip")
     dst_zip_path  = dst_path / filename
+    if dst_zip_path.exists():
+        try:
+            os.remove(dst_zip_path)
+        except Exception as e:
+            print(f"Error removing existing zip file '{dst_zip_path}': {e}")
+            return
 
     try:
+        total_files = sum(len(files) for _, _, files in os.walk(src_path))
         with zipfile.ZipFile(dst_zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-            total_files = sum([len(files) for r, d, files in os.walk(src_path)])
-            with tqdm(total=total_files, desc=f"Zipping '{filename}'") as pbar:
+            with tqdm(total=total_files, desc=f"* Zipping '{filename}'") as pbar:
                 for dirpath, dirnames, filenames in os.walk(src_path):
                     for filename in filenames:
                         file_path = Path(dirpath) / filename
@@ -88,6 +92,76 @@ def _backup(src_path, dst_path):
                         pbar.update(1)
     except Exception as e:
         print(f"Error during backup: {e}")
+
+def _folder_has_changes(path):
+    """
+    Check if a folder has changed based on the SHA256 hash of its contents.
+
+    Args:
+        path (str or Path): Path to the folder to check.
+
+    Returns:
+        bool: True if the folder has changed, False otherwise.
+    """
+
+    hash_sha256 = hashlib.sha256()
+    for dir_path, _, filenames in sorted(os.walk(path)):
+        dir_path = Path(dir_path)
+
+        for filename in sorted(filenames):
+            file_path = dir_path / filename
+
+            try:
+                if not file_path.exists():
+                    continue
+
+                # Add relative path and file size to the hash
+                rel_path = str(file_path.relative_to(path))
+                hash_sha256.update(rel_path.encode())
+                hash_sha256.update(str(file_path.stat().st_size).encode())
+
+                # Add file content to the hash
+                # with open(file_path, 'rb') as f:
+                #     while chunk := f.read(8192):
+                #         hash_sha256.update(chunk)
+
+            except FileNotFoundError:
+                print(f"File not found: {file_path}. Skipping...")
+            except PermissionError:
+                print(f"Permission denied: {file_path}. Skipping...")
+            except Exception as e:
+                print(f"Error processing file {file_path}: {e}. Skipping...")
+
+    hash_value = hash_sha256.hexdigest()
+
+    filename = _convert_to_filename(path, "txt")
+    hash_path = config.get_hashes_path() / filename
+
+    if hash_path.exists():
+        with open(hash_path, 'r') as f:
+            stored_hash = f.read().strip()
+        if stored_hash == hash_value:
+            return False
+
+    with open(hash_path, 'w') as f:
+        f.write(hash_value)
+    return True
+
+def _convert_to_filename(path, filetype):
+    if path == None:
+        print("backup_folder.py => No path given, cannot convert.")
+        return
+    if filetype == None:
+        print("backup_folder.py => No filetype given, cannot convert.")
+        return
+
+    filename = str(path)
+    filename = filename.replace("\\", "_").replace("/", "_").replace("__", "_")
+    filename = _cleanup_path_string(filename)
+
+    filetype = filetype.replace(".","")
+
+    return f"{filename}.{filetype}"
 
 def _cleanup_path_string(path_str):
     symbols = ["\\", "/", ":", " ", "."]
