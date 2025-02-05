96941c59e0aa34352294733bec81c4238a7b9a8d
ImEldie <daniel.frankort@telenet.be>
2025-02-05 14:47:21 +0100

    Fix Packages for easier install

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'bup-cli',
     version = '1.0.0',
-    packages = ['bupcli'],
+    packages = find_packages(),
     entry_points = {
         'console_scripts': [
             'bup = bupcli.__main__:main'
